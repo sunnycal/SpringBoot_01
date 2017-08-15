7d6000011c8b51e76bffd9625a3f21adfa6c9bf5
sunnycal <milana.campbell@gmail.com>
2017-08-15 12:52:16 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String homePage(){
+        return "index";
+    }
+}
